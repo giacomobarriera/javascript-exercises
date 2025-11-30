d2d8b9e529d05b24c9f71648510e77a02559d1e0
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-11-30 21:58:45 +0100

    Aggiunto il loop e trasformato array in string.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+   
+    let arr = [];
 
-};
+    for (let i = 0; i < num; i++) {
+        arr.push(string);
+    }
+
+    let arrString = arr.join()
+
+    return arrString.replaceAll(",", "")
+}
+
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
