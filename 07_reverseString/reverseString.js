4d917fe400fbf36c8f1d6cafe06b680a0560af69
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-12-01 19:33:06 +0100

    Completata funzione reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let str = "";
+    let arr = string.split("");
+    let reverseArr = arr.reverse();
 
+    for (let char of reverseArr) {
+        str += char ;
+    }
+    return str;
 };
 
 // Do not edit below this line
