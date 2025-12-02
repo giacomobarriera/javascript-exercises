c570b0e8e38a515c5b0eabbf652e976fa532aad0
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-12-02 05:12:09 +0100

    removesArray passato primo test.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, obj) {
+        let index = arr.indexOf(obj)
 
+        arr.splice(index, 1)
+
+        return arr;
+};
+ 
 // Do not edit below this line
 module.exports = removeFromArray;
