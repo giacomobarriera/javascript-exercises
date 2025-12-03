56ab29c28eaa5abf682d6442e4378cf4c965fafb
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-12-03 05:23:06 +0100

    Implementata la funzione per creare il nuovo array.
    A e b saranno il min e il max dell'array.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+
+// prendi i due arg della funzione
+const sumAll = function(a, b) {
+//crea un nuovo array da riempire
+    let newArr = []
+//comincia un loop al contrario
+//usa a e b come max e min
+    for (let i = b; i > a; i--) {
+//spingi l'indice del loop nel newArr
+        newArr.push(i);
+    }
+//somma tutti gli elementi
+    return newArr;;
+
+//per ogni numero somma gli elementi
+
+
+
 
 };
 
