56cdc1ee110b4e795b12f4409172eb194e468819
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-12-03 05:55:14 +0100

    Aggiunta condizione positività di a o be
    Se a o b sono negativi allora verrà segnalato ERRORE

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -4,7 +4,13 @@ const sumAll = function(a, b) {
 //crea un nuovo array da riempire
     let newArr = []
 //comincia un loop al contrario
-//usa a e b come max e min
+
+//se a o b sono negativi
+if (a < 0 || b < 0) {
+// segnala ERRORE
+    return "ERROR"
+// se a o b sono positivi
+} else { 
 //se a più grande di b
     if (a > b) {
 //se b più grande di a
@@ -12,6 +18,7 @@ const sumAll = function(a, b) {
 //spingi l'indice del loop nel newArr
             newArr.push(i);
         }
+// se a più grande di b
     } else {
         for (let i = b; i >= a; i--) {
 //spingi l'indice del loop nel newArr
@@ -24,7 +31,7 @@ const sumAll = function(a, b) {
     });
 
 //per ogni numero somma gli elementi
-
+}
 };
 
 // Do not edit below this line
