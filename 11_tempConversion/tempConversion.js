f183b9ce5d050e8c4f01bfa1f2116820d46b68c5
Jacki <230686940+giacomobarriera@users.noreply.github.com>
2025-12-04 22:02:28 +0100

    Aggiunte condizioni e funzioni
    Le stringhe verranno trasformate in numeri.
    I numeri interi senza virgola
    I numeri decimali arrotondati alla prima cifra.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,24 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  const celsius = (fahr - 32) * (5/9);
+
+  //se numero intero
+  if (celsius == Math.floor(celsius))
+    return parseFloat(celsius);
+  //se numero decimale
+  else  
+    return parseFloat(celsius.toFixed(1));
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  const fahreneit = cels * (9/5) + 32
+  
+  //se numero intero
+  if (fahreneit == Math.floor(fahreneit))
+    return parseFloat(fahreneit);
+  //se numero decimale
+  else
+    return parseFloat(fahreneit.toFixed(1));
 };
 
 // Do not edit below this line
